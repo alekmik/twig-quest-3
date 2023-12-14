e0b8a81303f1fa88bb9ff91b9edfc6d4856d465e
Alek Miko <alekmikopro@gmail.com>
2023-12-14 23:14:16 +0100

    add of a global, the global is called in the following pages : 'index.php' & 'details.php'

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale emailContactCorp à l'initialisation de l'objet Twig
+$emailContactCorp = 'twig_is_fun@hotmail.es';
+$twig->addGlobal('contactEmail', $emailContactCorp);
